e9e91e0c807c1997761d7763f7ca5629270cacb1
MishelleBohorquez <dmishellebohorquez@ucundinamarca.edu.co>
2026-07-14 21:10:07 -0500

    Modúlos

---
+++ modulos/producto.js
@@ -0,0 +1,15 @@
+class Producto {
+
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+
+  }
+
+  calcularTotal() {
+    return this.precio * 1.19;
+  }
+
+}
+
+module.exports = Producto
---
+++ modulos/usuario.js
@@ -0,0 +1,14 @@
+class Usuario {
+
+  constructor(nombre, email) {
+    this.nombre = nombre;
+    this.email  = email;
+  }
+
+  saludo() {
+    return "Hola, soy " + this.nombre;
+  }
+
+}
+
+module.exports = Usuario;
